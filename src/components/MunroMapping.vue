b680a7369872ee571339c5fedaffb107e9557f82
DavidTaylor96 <david_taylor96@outlook.com>
2021-02-28 15:27:44 +0000

    Trying to Add Map

---
+++ src/components/MunroMapping.vue
@@ -0,0 +1,41 @@
+<template>
+ <div id="container">
+   <div id="mapContainer"></div>
+ </div>
+</template>
+
+<script>
+import "leaflet/dist/leaflet.css";
+import L from "leaflet";
+
+export default {
+ name: "Map",
+ data() {
+   return{
+    center: [57.070368, -3.669099]
+   }},
+ methods: {
+   setupLeafletMap: function () {
+     const mapDiv = L.map("mapContainer").setView(this.center, 13);
+     L.tileLayer(
+       "https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}",
+       {
+         attribution:
+           'Map data (c) <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
+         maxZoom: 18,
+         id: "mapbox/streets-v11",
+         accessToken:”XXX",
+       }
+     ).addTo(mapDiv);
+   },
+ },
+ mounted() {
+   this.setupLeafletMap();
+ },
+};
+
+</script>
+
+<style>
+
+</style>
